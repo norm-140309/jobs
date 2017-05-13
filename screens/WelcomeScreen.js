4017b8510460de32d741bb6fbae88ab39c92c7bb
Norm Glomski <nglomski@gmail.com>
2017-05-13 18:02:42 -0500

    Tab Navigators were setup

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 
 class WelcomeScreen extends Component {
-  render(){
+
+
+  render() {
     return (
       <View>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
         <Text>WelcomeScreen</Text>
         <Text>WelcomeScreen</Text>
         <Text>WelcomeScreen</Text>
